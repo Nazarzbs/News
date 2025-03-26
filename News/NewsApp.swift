c310b21577aca4b18c74a64fb996c90ddcb74a9b
Nazar <famina1998@gmail.com>
2025-03-26 18:48:02 +0200

    Initial Commit

---
+++ News/NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsApp.swift
+//  News
+//
+//  Created by Nazar on 26/3/25.
+//
+
+import SwiftUI
+
+@main
+struct NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
